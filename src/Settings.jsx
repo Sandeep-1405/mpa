cd9dfddb5418c3255235d76282c483365eaf89c5
Sandeep <sandeepmaraboina14s@gmail.com>
2025-05-20 13:47:33 +0530

    2

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const Settings = () => {
   return (
     <div>
+      <Helmet>
+        <title>Settings</title>
+      </Helmet>
       <h1>Settings</h1>
       <Link to="/">Go to home</Link>
     </div>
