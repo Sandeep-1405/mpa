7c1bc59acb18cacd2fab79ef8373fb3af237d0ee
Sandeep <sandeepmaraboina14s@gmail.com>
2025-05-20 13:41:40 +0530

    first

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <h1>Home mpa</h1>
+      <Link to="/settings">Go to settings</Link>
+    </div>
+  );
+};
+
+export default Home;
